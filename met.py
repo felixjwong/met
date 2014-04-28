d36945236a392b85d45c19ed768815a3f5bfa1a5
Felix Wong <Felix@Felixs-MacBook-Air.local>
2014-04-27 20:17:40 -0400

    new processing script

--- met.py
+++ met.py
@@ -15,28 +15,81 @@
 ## get new workout data
 import sys
 import subprocess
-p = subprocess.Popen(['ls', '-l'], stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
+p = subprocess.Popen(['curl', "'https://api.spark.io/v1/devices/"+sys.argv[1]+"/temp?access_token="+sys.argv[2]+"'", ">", sys.argv[1]+"/"+str(i)+".txt"], stdout=subprocess.PIPE,
+stderr=subprocess.PIPE)
 out, err = p.communicate()
 
 print out
 print sys.argv[0], sys.argv[1]
 
 
-# curl "https://api.spark.io/v1/devices/51ff66065067545736270187/temp?access_token=be2f2d8cd0cbd6844f3ff7871b86624f7bf1c505"
+#   > sparkid/1.txt
+
+
+# eliminate redundant data
+
+#_________
 
 
 
 
 
+
+
+
+
+
+#51ff66065067545736270187
 
 
 ## retraining using ALL data
 
 
+1. read in everyhting from sparkid/*
+2. data = [everything]
+(time, heart rate, runing, avg)
+
+regression/
+function optimization
 
 
+optimalrun =
+[ (30, heart rate, runing, avg)
+ (60, heart rate, runing, avg)
+ (90, heart rate, runing, avg)
+ (time, heart rate, runing, avg)
+ (time, heart rate, runing, avg)]
+
+
+write optimalrun sparkid/optimal.txt
+
+# plot all data,
+# visual optimal agaisnt distribution of other runs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---==-
+# sparkid/optimal.txt
 
 ## push out new routine to spark cloud
+# put optimal.txt onto spark cloud
+
+
+
+
+
 
 
--- processing.py
+++ processing.py
@@ -1,201 +1,269 @@
 #!/usr/bin/env python
-# data.py - for python 2.7+
+# processing.py - for python 2.7+
 # Version 1.0
 # MET project by Chih-Yu (Andy) Liu, Tracy Lu, and Felix Wong
 
-# Usage: 'python data.py data.txt' at the command line
-# Output: random testing numbers
+# Usage: 'python processing.py [sparkid]' at the command line
+# Sample Usage: 'python processing.py 51ff66065067545736270187'
+# Output: optimal heart-rate running pace timeseries as array
 #-----------------------------------------------------------------------------#
 
 # packages
-#from __future__ import print_function
+# from __future__ import print_function
+from __future__ import division
 import sys
-import matplotlib.pyplot as plt
-
-currenttime = eval(sys.argv[1])
-timerange = 10
-numberofdatafiles = 20
-
-
-dataframe = []
-# input graphs from files specified in command line and eliminate spaces
-for inputfile in range(1,numberofdatafiles+1):
-    z = []
-    fopen = open('./data/'+str(inputfile)+'.txt');
-    for line in fopen.readlines():
-        z.append([eval(item) for item in line.split('\t')])
-    fopen.close()
-    dataframe.append(z)
-
-# the variable data has all the information, in
-# [
-#   [
-#   [timestep,heartrate,pedo,temp],[timestep,heartrate,pedo,temp],[timestep,heartrate,pedo,temp]...
-#   ],
-#   ........,[....],......
-# ]
-
-
-import math
-from matplotlib import pyplot
-import pylab
-from mpl_toolkits.mplot3d import Axes3D
-import random
-
-
-fig = pylab.figure()
-ax = Axes3D(fig)
-
-
-flat = [item for sublist in dataframe for item in sublist]
-timedata = []
-#timedata = [i for i in flat if abs(i[0]-currenttime) <= timerange]
-for i in flat:
-    if abs(i[0]-currenttime) <= timerange:
-        if i[0] == currenttime - timerange:
-            normalize =i[4]
-        timedata.append([i[0],i[1],i[2],i[3],i[4]-normalize])
-print timedata
-
-
-x1_var = [y for [x,y,z,w,v] in timedata]
-x2_var = [z for [x,y,z,w,v] in timedata]
-x3_var = [w for [x,y,z,w,v] in timedata]
-d_var = [v for [x,y,z,w,v] in timedata]
-t_var = [x for [x,y,z,w,v] in timedata]
-
-
-ax.scatter(x1_var, x2_var, x3_var)
-
-
-
+import os, os.path
 import numpy as np
-X = np.array([(x1_var[i], x2_var[i],x3_var[i]) for i in range(len(timedata))])
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from scipy.optimize import fsolve
+
+directory = './data/'+sys.argv[1]+'/'
+alldatafiles = [item for item in os.listdir(directory) if '.txt' in item]
+
+# run FANCY PLOTS MODULE?
+fancyplots = 1
+# run REGRESSION PLOTING MODULE?
+regressionplots = 1
+
+
+#--------------------------DATA IMPORT MODULE--------------------------------#
+
+# time, heart rate, inst. running rate, avg. running rate arrays
+ti = []
+hr = []
+rr = []
+ar = []
+mean_ar = []
+
+# input graphs from files specified in command line and eliminate spaces
+for inputfile in alldatafiles:
+    fopen = open(directory+inputfile);
+    yy = []
+    for line in fopen.readlines():
+        currentline = line.split('\t')
+        ti.append(eval(currentline[0]))
+        hr.append(eval(currentline[1]))
+        rr.append(eval(currentline[2]))
+        ar.append(eval(currentline[3]))
+        yy.append(eval(currentline[3]))
+    mean_ar.append(np.mean(yy))
+    fopen.close()
+
+ti_np = np.array(ti)
+hr_np = np.array(hr)
+rr_np = np.array(rr)
+ar_np = np.array(ar)
+# the variables ti, hr, rr, ar store the time series of each variable
 
 
 
 
+#--------------------------FANCY PLOTS MODULE--------------------------------#
 
+if fancyplots == 1:
+    # histogram of average running rates
+    p0 = plt.figure(0)
+    plt.hist(mean_ar, bins=10, color='blue')
+    plt.title('Distribution of Average Running Rate Workouts')
+    plt.xlabel('Average Running Rate During Routine')
+    plt.ylabel('Frequency')
 
-from pylab import plot,show
-from numpy import vstack,array
-from numpy.random import rand
-from scipy.cluster.vq import kmeans,vq
-
-# data generation
-
-# computing K-Means with K = 2 (2 clusters)
-centroids,_ = kmeans(X,5)
-# assign each sample to a cluster
-idx,_ = vq(X,centroids)
-
-
-ax.scatter(x1_var, x2_var, x3_var,s = .1,facecolors='none', edgecolors='b')
-ax.scatter(centroids[:,0],centroids[:,1],centroids[:,2],marker='D', color='g')
-
-
-
-
-
-
-
-
-
-
-
-from scipy.spatial import ConvexHull
-
-hull = ConvexHull(X)
-hull_points = hull.vertices
-
-Y = hull.points[np.unique(hull.simplices)]
-
-y1_var = [x for [x,y,z] in Y]
-y2_var = [y for [x,y,z] in Y]
-y3_var = [z for [x,y,z] in Y]
-
-
-
-yd_var = []
-yt_var = []
-for i in np.unique(hull.simplices):
-    yd_var.append(d_var[i])
-    yt_var.append(t_var[i])
-
-
-drange = max(yd_var)-min(yd_var)
-tmap = [float(yd_var[i]*100000/drange/yt_var[i]) for i in range(len(yd_var))]
-
-for i in range(0,len(y1_var)):
-    print yt_var[i],y1_var[i], y2_var[i], y3_var[i],yd_var[i],tmap[i]
-
-ax.scatter(y1_var, y2_var, y3_var, marker = '*',s = 150, c=tmap, cmap=plt.cm.jet)
-pyplot.show()
-
-
-
-
-
-
-
-
-
-
-
-"""
-# some plotting using numpy's logical indexing
-plot(data[idx==0,0],data[idx==0,1],'ob',
-     data[idx==1,0],data[idx==1,1],'or')
-plot(centroids[:,0],centroids[:,1],'sg',markersize=8)
-show()
-"""
-
-
-
-
-"""
-    
-    #  heartrate + temperature for (1.txt 2.txt data in time range)
-    a = plt.figure(1)
-    dataplot = [[y,z] for [x,y,z,w,v] in timedata]
-    plt.scatter(*zip(*dataplot),s=2,marker='o', color='r')
-    a.show()
-    raw_input()
-    
-    
-    
-    
-    # timestep vs heartrate for (1.txt 2.txt data)
-    a = plt.figure(1)
-    dataplot = [[x,y] for [x,y,z,w] in dataframe[0]]
+    # pairwise time series of running factors
+    # ti vs hr
+    p1 = plt.figure(1)
+    dataplot = zip(ti_np,hr_np)
     plt.scatter(*zip(*dataplot),s=2)
-    dataplot = [[x,y] for [x,y,z,w] in dataframe[1]]
-    plt.scatter(*zip(*dataplot),s=2,marker='o', color='r')
-    
-    # timestep vs pedo
-    b = plt.figure(2)
-    dataplot = [[x,z] for [x,y,z,w] in dataframe[0]]
+    plt.title('Aggregated Data: Time vs. Heart Rate')
+    plt.xlabel('Time')
+    plt.ylabel('Heart Rate')
+
+    # ti vs ar
+    p2 = plt.figure(2)
+    dataplot = zip(ti_np,ar_np)
     plt.scatter(*zip(*dataplot),s=2)
-    dataplot = [[x,z] for [x,y,z,w] in dataframe[1]]
-    plt.scatter(*zip(*dataplot),s=2,marker='o', color='r')
-    
-    # timestep vs temp
-    c = plt.figure(3)
-    dataplot = [[x,w] for [x,y,z,w] in dataframe[0]]
+    plt.title('Aggregated Data: Time vs. Average Running Rate')
+    plt.xlabel('Time')
+    plt.ylabel('Average Running Rate')
+
+    # hr vs ar
+    p3 = plt.figure(3)
+    dataplot = zip(hr_np,ar_np)
     plt.scatter(*zip(*dataplot),s=2)
-    dataplot = [[x,w] for [x,y,z,w] in dataframe[1]]
-    plt.scatter(*zip(*dataplot),s=2,marker='o', color='r')
-    
+    plt.title('Aggregated Data: Heart Rate vs. Average Running Rate')
+    plt.xlabel('Heart Rate')
+    plt.ylabel('Average Running Rate')
+
+    # rr vs ar
+    p4 = plt.figure(4)
+    dataplot = zip(rr_np,ar_np)
+    plt.scatter(*zip(*dataplot),s=2)
+    plt.title('Aggregated Data: Instantaneous Running Rate vs. Average Running Rate')
+    plt.xlabel('Instantaneous Running Rate')
+    plt.ylabel('Average Running Rate')
+
+    # hr vs rr
+    p5 = plt.figure(5)
+    dataplot = zip(hr_np,rr_np)
+    plt.scatter(*zip(*dataplot),s=2)
+    plt.title('Aggregated Data: Heart Rate vs. Instantaneous Running Rate')
+    plt.xlabel('Heart Rate')
+    plt.ylabel('Instantaneous Running Rate')
+
     # show plots
-    a.show()
-    b.show()
-    c.show()
+    p0.show()
+    p1.show()
+    p2.show()
+    p3.show()
+    p4.show()
+    p5.show()
     raw_input()
-    
-"""
 
 
 
+
+#----------------------------REGRESSION MODULE--------------------------#
+
+
+# Use scipy.curve_fit for fitting to predefined functions
+
+# hr vs rr: returns rr=function(hr)
+# exponential fit done by taking log first
+def func1(x, a, b):
+    return a*x+b
+params = curve_fit(func1, hr_np, np.log(rr_np))
+[a1,b1]=params[0]
+print params[0]
+
+# rr vs ar: returns ar=function(rr)
+def func2(x, a, b):
+    return a*x**2+b
+params = curve_fit(func2, rr_np, ar_np)
+[a2,b2]=params[0]
+print params[0]
+
+# hr vs ar: returns ar=function(hr)
+# exponential fit done by taking log first
+def func3(x, a, b):
+    return a*x+b
+params = curve_fit(func3, hr_np, np.log(ar_np))
+[a3,b3]=params[0]
+print params[0]
+
+# ti vs ar: returns ar=function(ti)
+def func4(x, a, b):
+    return a*x+b
+params = curve_fit(func4, ti_np, ar_np)
+[a4,b4]=params[0]
+print params[0]
+
+
+
+
+
+#-----------------------REGRESSION PLOTTING MODULE--------------------------#
+
+if regressionplots == 1:
+    # plot all of the regressions above
+    # hr vs rr
+    x = np.linspace(min(hr),max(hr),100)
+    y = func1(x, a1, b1)
+    p7 = plt.figure(7)
+    dataplot = zip(hr_np,rr_np)
+    plt.scatter(*zip(*dataplot),s=2)
+    plt.plot(x,np.exp(y))
+    plt.title('Fitted Data: Heart Rate vs. Instantaneous Running Rate')
+    plt.xlabel('Heart Rate')
+    plt.ylabel('Instantaneous Running Rate')
+
+    # rr vs ar
+    x = np.linspace(min(rr),max(rr),100)
+    y = func2(x, a2, b2)
+    p8 = plt.figure(8)
+    dataplot = zip(rr_np,ar_np)
+    plt.scatter(*zip(*dataplot),s=2)
+    plt.plot(x,y)
+    plt.title('Fitted Data: Instantaneous Running Rate vs. Average Running Rate')
+    plt.xlabel('Instantaneous Running Rate')
+    plt.ylabel('Average Running Rate')
+
+    # hr vs ar
+    x = np.linspace(min(hr),max(hr),100)
+    y = func3(x, a3, b3)
+    p9 = plt.figure(9)
+    dataplot = zip(hr_np,ar_np)
+    plt.scatter(*zip(*dataplot),s=2)
+    plt.plot(x,np.exp(y))
+    plt.title('Fitted Data: Heart Rate vs. Average Running Rate')
+    plt.xlabel('Heart Rate')
+    plt.ylabel('Average Running Rate')
+
+    # ti vs ar
+    x = np.linspace(min(ti),max(ti),100)
+    y = func4(x, a4, b4)
+    p10 = plt.figure(10)
+    dataplot = zip(ti_np,ar_np)
+    plt.scatter(*zip(*dataplot),s=2)
+    plt.plot(x,y)
+    plt.title('Fitted Data: Time vs. Average Running Rate')
+    plt.xlabel('Time')
+    plt.ylabel('Average Running Rate')
+
+    # show plots
+    plt.close(0)
+    plt.close(1)
+    plt.close(2)
+    plt.close(3)
+    plt.close(4)
+    plt.close(5)
+    plt.close(6)
+    p7.show()
+    p8.show()
+    p9.show()
+    p10.show()
+    raw_input()
+
+
+
+
+#--------------------LEAST SQUARES OPTIMIZATION MODULE-----------------------#
+
+# set up energy function
+def LS_energy(t,h,w1,w2,w3,a1,a2,a3,a4,b1,b2,b3,b4):
+    return w3*func2(np.exp(func1(h,a1,b1)),a2,b2)+w2*np.exp(func3(h,a3,b3))+w1*func4(t,a4,b4)
+
+def constraint1(h,havg):
+    return abs(h-havg)
+           
+def constraint2(h,ravg):
+    return abs(np.exp(func1(h,a1,b1)-ravg))
+
+def total_energy(t,h,objA,c1,c2,c3,havg,ravg,w1,w2,w3,a1,a2,a3,a4,b1,b2,b3,b4):
+    return c1*abs(objA-LS_energy(t,h,w1,w2,w3,a1,a2,a3,a4,b1,b2,b3,b4))**2+c2*constraint1(h,havg)+c3*constraint2(h,ravg)
+
+# real parameters
+objA = 10
+havg = np.mean(hr_np)
+ravg = np.mean(rr_np)
+
+# weights
+w1 = 1/3
+w2 = 1/3
+w3 = 1/3
+c1 = 1
+c2 = 1
+c3 = 1
+
+#print LS_energy(100,100,w1,w2,w3,a1,a2,a3,a4,b1,b2,b3,b4)
+#print LS_energy(100,200,w1,w2,w3,a1,a2,a3,a4,b1,b2,b3,b4)
+print total_energy(10,100,objA,c1,c2,c3,havg,ravg,w1,w2,w3,a1,a2,a3,a4,b1,b2,b3,b4)
+
+# print out solutions to linear equation (heart rate)
+for time in range(1,1000):
+    f = lambda h:total_energy(time,h,objA,c1,c2,c3,havg,ravg,w1,w2,w3,a1,a2,a3,a4,b1,b2,b3,b4)
+    opth = fsolve(f, 50.0)[0]
+    # (optimal heart rate, optimal running rate) pair
+    print opth,np.exp(func1(opth,a1,b1))
 
 
 
